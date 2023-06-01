d500e6dd6535ffb0708f9e2820666c6b758faa18
Duckweeds7 <root@duckweeds7.com>
2023-06-02 06:58:18 +0800

    The loading bar has been removed, waiting for the image to load by displaying a wait, while modifying the corresponding xpath rules

--- cygnusx1/config.py
+++ cygnusx1/config.py
@@ -1,4 +1,4 @@
 GOOGLE_SUGGEST_CLASS = "F9PbJd"
 GOOGLE_THUBNAILS_XPATH = '//img[contains(@class, "rg_i")]'
 GOOGLE_IMAGE_LOADING_BAR_XPATH = '//*/div[contains(@class, "tvh9oe")][2]//div[@class="k7O2sd"]'
-GOOGLE_IMAGE_FULLSIZE_XPATH = '//*/div[contains(@class, "tvh9oe")][2]//img[contains(@class, "n3VNCb")]'
+GOOGLE_IMAGE_FULLSIZE_XPATH = '//*/div[contains(@class, "tvh9oe")][2]//img[contains(@class, "r48jcc pT0Scc iPVvYb")]'
--- cygnusx1/scraper.py
+++ cygnusx1/scraper.py
@@ -1,14 +1,15 @@
+import time
 from typing import List, Tuple
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from tqdm import tqdm
 from tqdm.contrib.concurrent import thread_map
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 
 from cygnusx1.config import (GOOGLE_SUGGEST_CLASS, GOOGLE_THUBNAILS_XPATH,
-                             GOOGLE_IMAGE_FULLSIZE_XPATH, GOOGLE_IMAGE_LOADING_BAR_XPATH)
+                             GOOGLE_IMAGE_FULLSIZE_XPATH)
 from cygnusx1.helper import get_browser, highlight
-
-import time
 
 
 def _get_google_suggests(root_url: str, headless: bool = False) -> List[str]:
@@ -57,16 +58,15 @@ def _scrap_google_page_image_urls(page_url: str, thread_id: int, headless: bool 
             try:
                 highlight(browser, thumbnail)
                 thumbnail.click()
-                loading_bar = browser.find_element(By.XPATH, GOOGLE_IMAGE_LOADING_BAR_XPATH)
-                # wait.until(WaitLoad(loading_bar))
-                if "display" in (str(loading_bar.get_attribute('style'))):
-                    wait.until(lambda _: 'display: none' in str(loading_bar.get_attribute('style')))
-                # It seems that the above code doesn't work, and all the downloads are thumbnails.
-                # The following code has been tested to work properly
-                while 'display: none' not in str(loading_bar.get_attribute('style')):
-                    time.sleep(1)
-                images = browser.find_elements(By.XPATH, GOOGLE_IMAGE_FULLSIZE_XPATH)
+                # Set the timeout for explicit waiting
+                wait_timeout = 3
+
+                # Wait for the element to be visible or exist for a period of time before searching
+                wait = WebDriverWait(browser, wait_timeout)
+                images = wait.until(EC.visibility_of_all_elements_located((By.XPATH, GOOGLE_IMAGE_FULLSIZE_XPATH)))
+
                 for img in images:
+                    # Usually, images [-1] are the ones with higher image quality. In my code, I took images [-1]
                     highlight(browser, img)
                     src = img.get_attribute('src')
                     image_srcs.add(src)
@@ -91,7 +91,7 @@ def scrap_google_images(args, keywork: str) -> Tuple[List[str], int]:
     for links, num_search_results in thread_map(_scrap_google_page_image_urls,
                                                 page_urls,
                                                 range(len(page_urls)),
-                                                [args.headless]*len(page_urls),
+                                                [args.headless] * len(page_urls),
                                                 max_workers=args.workers, desc="Page Image Scraping",
                                                 leave=False, colour='blue'):
         img_srcs.update(links)
